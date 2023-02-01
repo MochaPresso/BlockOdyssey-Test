4bf2d678336afd28eb00c002d7bec18a50575cc8
byeongseok <hallabong0126@gmail.com>
2023-02-01 20:19:37 +0900

    ✨ Feat: List Component search event 구현

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
 import "../styles/List.scss";
 
 const List = () => {
   const [productList, setProductList] = useState(null);
+  const { searchCondition, searchKeyword } = useSelector((state) => state.currentState);
+  const filteredProductList =
+    searchKeyword.length === 0
+      ? productList?.products
+      : searchCondition === "all"
+      ? productList?.products.filter((product) => {
+          const condition = [
+            product.title.toLowerCase(),
+            product.brand.toLowerCase(),
+            product.description.toLowerCase(),
+          ];
+
+          for (const elem of condition) {
+            if (elem.includes(searchKeyword)) {
+              return true;
+            }
+          }
+
+          return false;
+        })
+      : productList?.products.filter((product) =>
+          product[searchCondition].toLowerCase().includes(searchKeyword)
+        );
 
   useEffect(() => {
     (async () => {
@@ -18,7 +42,9 @@ const List = () => {
 
   return (
     <div className="List">
-      <div className="List-total">검색된 데이터: {productList && productList.total}건</div>
+      <div className="List-total">
+        검색된 데이터: {filteredProductList && filteredProductList.length}건
+      </div>
       <div className="List-table">
         <div className="List-table-head">
           <div>상품 번호</div>
@@ -30,8 +56,8 @@ const List = () => {
           <div>재고</div>
         </div>
         <div className="List-table-body">
-          {productList &&
-            productList.products.map(({ id, title, brand, description, price, rating, stock }) => (
+          {filteredProductList &&
+            filteredProductList.map(({ id, title, brand, description, price, rating, stock }) => (
               <div className="List-table-content" key={id}>
                 <div>{id}</div>
                 <div>{title}</div>
